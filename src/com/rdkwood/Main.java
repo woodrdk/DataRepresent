71e74e1a00d0df0a17b4034622722b5ef4adfaa7
Robert Wood Jr <rdrwood@gmail.com>
2020-04-08 11:10:20 -0700

    Initial commit

---
+++ src/com/rdkwood/Main.java
@@ -0,0 +1,60 @@
+package com.rdkwood;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // check our recap anwsers
+        // ints have 4 bytes
+
+        int data = 0b01100011;
+        System.out.println("Base 10 (Decimal Value) is " + data);
+
+        int data2 = 0b10110110;
+        System.out.println("Octal " + Integer.toOctalString(data2));
+        int data3 = 0b00111111;
+        System.out.println("Hex " + Integer.toHexString(data3));
+
+        intDataType();
+        byteDataType();
+        longDataType();
+    }
+
+    // By default the byte data type store numbers in the range -128 to 127
+    // using twos integer numbers
+    private static void byteDataType() {
+
+    }
+
+    
+    private static void longDataType() {
+
+    }
+
+    // this method will play with the in data type
+    // int is using 4 bytes to store numbers as twos complements numbers
+    public static void intDataType(){
+        int max = Integer.MAX_VALUE;
+        int min = Integer.MIN_VALUE;
+
+        System.out.println("Max int: " + max);
+        System.out.println("Min int: " + min);
+
+        // add 1 to max
+        max++;
+        System.out.println("Max Int overflowed " + max);
+
+        // assign a hex number (0x) to the into
+        int num = 0xAFFAA23C;
+        System.out.println("Num is " + num);
+
+        // assign an octal number (0) to the int
+        int hexNum = 0721;
+        System.out.println("Num is " + hexNum);
+
+        // assign a binary number (0b)
+        int binNum = 0b11000110;
+        System.out.println("Num as Binary: " + Integer.toBinaryString(binNum));
+
+    }
+}
